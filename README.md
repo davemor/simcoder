0d3a7f42c6aef25604d4f58ae0801b2b669e8909
David Morrison <dm236@st-andrews.ac.uk>
2023-03-20 09:51:45 +0000

    Fix chunking

---
+++ README.md
@@ -0,0 +1,18 @@
+# Simcoder
+A utility for encoding images using the SimCLR2 pretrained model weigths.
+
+# Installation
+The tool is designed to run inside a docker container and requires a build step to use. There are a set of make targets that automate these steps. They are in the Makefile. You may need to modify them for your machine.
+
+Here is an example of installation:
+
+```bash
+git clone https://github.com/davemor/simcoder.git
+cd simcoder
+make docker_image
+```
+
+# Usage
+```bash
+
+```
--- simcoder/__main__.py
+++ simcoder/__main__.py
@@ -1,4 +1,5 @@
 import logging
+from pathlib import Path
 
 import click
 import numpy as np
@@ -30,7 +31,7 @@ def get_resnet50():
     return model
 
 
-def get_simclr2(model_name):
+def get_simclr2():
     saved_model_path = (
         "gs://simclr-checkpoints-tf2/simclrv2/finetuned_100pct/r50_1x_sk0/saved_model/"
     )
@@ -42,13 +43,13 @@ def get_simclr2(model_name):
 def save_array(arr, output_path, format):
     logging.info(f"Saving embeddings to {output_path}.")
 
-    save_path = output_path.with_suffix(format)
+    save_path = output_path.with_suffix(f".{format}")
 
-    if format == 'csv':
-        np.savetxt(save_path, arr, delimiter=',')
-    elif format == 'npy':
+    if format == "csv":
+        np.savetxt(save_path, arr, delimiter=",")
+    elif format == "npy":
         np.save(save_path, arr)
-    elif format == 'mat':
+    elif format == "mat":
         savemat(
             save_path,
             {"features": arr, "label": "simclr2_final_avg_pool_embeddings"},
@@ -58,10 +59,17 @@ def save_array(arr, output_path, format):
 
 
 @click.command()
-@click.argument('input_dir', type=click.Path(exists=False))
-@click.argument('output_path', type=click.Path(exists=False))
-@click.option('--format', type=click.Choice(['csv', 'npy', 'mat']), default='csv', help='output format')
-@click.option('--chunksize', type=int, default=10000, help='number of rows to process at a time')
+@click.argument("input_dir", type=click.Path(exists=False))
+@click.argument("output_path", type=click.Path(exists=False))
+@click.option(
+    "--format",
+    type=click.Choice(["csv", "npy", "mat"]),
+    default="csv",
+    help="output format",
+)
+@click.option(
+    "--chunksize", type=int, default=10000, help="number of rows to process at a time"
+)
 def encode(input_dir, output_path, format, chunksize):
     logging.info("Welcome to Simcoder.")
 
@@ -111,12 +119,14 @@ def encode(input_dir, output_path, format, chunksize):
     features = np.concatenate(fs, axis=0)
 
     if chunksize:
-        chunks = np.array_split(x, np.arange(chunksize, len(features), chunksize))
+        chunks = np.array_split(
+            features, np.arange(chunksize, len(features), chunksize)
+        )
         logging.info(len(chunks))
         for idx, chunk in enumerate(chunks):
-            save_array(chunk, output_path / f"{idx}")
+            save_array(chunk, Path(output_path) / f"{idx}", format)
     else:
-        save_array(features, output_path)
+        save_array(features, Path(output_path), format)
 
 
 if __name__ == "__main__":
